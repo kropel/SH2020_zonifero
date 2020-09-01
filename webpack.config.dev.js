a9ea856be98ad5594c0eba019d8e84922b115f05
Radek Kuczynski <krooopel@gmail.com>
2020-09-01 11:30:45 +0200

    warking environment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,13 @@
-const merge = require('webpack-merge');
-const webpackConfig = require('./webpack.config');
-
+const merge = require("webpack-merge");
+const webpackConfig = require("./webpack.config");
 module.exports = merge(webpackConfig, {
-
-    devtool: 'eval',
-
-    output: {
-        pathinfo: true,
-        publicPath: '/',
-        filename: '[name].js'
-    },
-
-    devServer: {
-        host: '0.0.0.0'
-    }
-
+  devtool: "eval",
+  output: {
+    pathinfo: true,
+    publicPath: "/",
+    filename: "[name].js",
+  },
+  devServer: {
+    host: "0.0.0.0",
+  },
 });
