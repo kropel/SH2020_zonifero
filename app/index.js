3d0c70382360ac34034eb36273b1ed5ea992fb1f
Radek Kuczynski <krooopel@gmail.com>
2020-09-03 13:05:39 +0200

    added features section with simple js functions

--- app/index.js
+++ app/index.js
@@ -3,8 +3,65 @@
  */
 
 // Load application styles
-import 'styles/index.scss';
+import "styles/index.scss";
 
 // ================================
 // START YOUR APP HERE
 // ================================
+(() => {
+  const featuresCards = [
+    {
+      title: "Hot-desk booking",
+      bgImage: "assets/images/mob_features_bg.png",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique velit id finibus convallis. Fusce eget purus aliquet libero euismod blandit sed eu enim. Curabitur eget arcu ut augue.",
+    },
+    {
+      title: "Status of employees",
+      bgImage:
+        "http://felixdepass.com/media/thumbnails/disegno-workspace/FelixdePass_DisegnoDinesen_Workspace_02_HR.jpg.1800x1200_q90_upscale.jpg",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique velit id finibus convallis. Fusce eget purus aliquet libero euismod blandit sed eu enim. Curabitur eget arcu ut augue.",
+    },
+    {
+      title: "Request management",
+      bgImage:
+        "http://felixdepass.com/media/thumbnails/disegno-workspace/FelixdePass_DisegnoDinesen_Workspace_20_HR.jpg.1800x1200_q90_upscale.jpg",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique velit id finibus convallis. Fusce eget purus aliquet libero euismod blandit sed eu enim. Curabitur eget arcu ut augue.",
+    },
+  ];
+
+  let currentCard = 0;
+  const leftArrow = document.querySelector(".features__cards__nav__arrow-left");
+  const rightArrow = document.querySelector(
+    ".features__cards__nav__arrow-right"
+  );
+  const titleHtml = document.querySelector(
+    ".features__cards__card__content__header"
+  );
+  const bgImageHtml = document.querySelector(
+    ".features__cards__card__images__background-image"
+  );
+  const contentHtml = document.querySelector(
+    ".features__cards__card__content__paragraph"
+  );
+
+  const setNewCard = () => {
+    const { title, bgImage, content } = featuresCards[
+      Math.abs(currentCard % featuresCards.length)
+    ];
+    titleHtml.textContent = title;
+    bgImageHtml.setAttribute("src", bgImage);
+    contentHtml.textContent = content;
+  };
+
+  leftArrow.addEventListener("click", (event) => {
+    currentCard--;
+    setNewCard();
+  });
+  rightArrow.addEventListener("click", (event) => {
+    currentCard++;
+    setNewCard();
+  });
+})();
